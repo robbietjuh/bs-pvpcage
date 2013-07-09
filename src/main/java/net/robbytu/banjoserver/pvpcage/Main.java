b513ab509d31a0e68516b765ce2bae9cc4fe245a
Robert de Vries <r.devries@robbytu.net>
2013-07-10 01:19:47 +0200

    Initial commit

---
+++ src/main/java/net/robbytu/banjoserver/pvpcage/Main.java
@@ -0,0 +1,19 @@
+package net.robbytu.banjoserver.pvpcage;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class Main extends JavaPlugin {
+    static Main plugin;
+
+    @Override
+    public void onEnable() {
+        this.plugin = this;
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
